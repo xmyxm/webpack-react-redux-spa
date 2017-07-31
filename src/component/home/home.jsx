8f53da02a3ecbc0c7c184933c6680ff651c22660
xmyxm <574322189@qq.com>
2017-08-01 01:26:46 +0800

    'add'

--- src/component/home/home.jsx
+++ src/component/home/home.jsx
@@ -14,21 +14,81 @@ export default class Home extends Component{
 				<ul className = "list" >
 					<li className = "item" >
 						<Link to="/detail/1" className = "title">sublime相关插件及其快捷键记录</Link>
+						<p className = "content">
+							快捷键：
+							1：在Sublime Text中，按下Ctrl+Shift+P调出命令面板;
+							2：输入install 调出 Install Package 选项并回车;
+							3：输入pretty，并在列表中选择HTML-CSS-JS Prettify后回车即可安装（格式化 javascript、css、html）
+						</p>
+						<div className = "information">
+							<span className = "time">2017-07-31</span>
+							<span className = "author">晨曦沐枫</span>
+						</div>
 					</li>
 					<li className = "item" >
 						<Link to="/detail/2" className = "title">您的安全性偏好设置仅允许安装来自 App Store 和被认可的开发者的应用</Link>
+						<p className = "content">
+							快捷键：
+							1：在Sublime Text中，按下Ctrl+Shift+P调出命令面板;
+							2：输入install 调出 Install Package 选项并回车;
+							3：输入pretty，并在列表中选择HTML-CSS-JS Prettify后回车即可安装（格式化 javascript、css、html）
+						</p>
+						<div className = "information">
+							<span className = "time">2017-07-31</span>
+							<span className = "author">晨曦沐枫</span>
+						</div>
 					</li>
 					<li className = "item" >
 						<Link to="/detail/3" className = "title">node升级到8.0.0在vscode启动js执行文件报错</Link>
+						<p className = "content">
+							快捷键：
+							1：在Sublime Text中，按下Ctrl+Shift+P调出命令面板;
+							2：输入install 调出 Install Package 选项并回车;
+							3：输入pretty，并在列表中选择HTML-CSS-JS Prettify后回车即可安装（格式化 javascript、css、html）
+						</p>
+						<div className = "information">
+							<span className = "time">2017-07-31</span>
+							<span className = "author">晨曦沐枫</span>
+						</div>
 					</li>
 					<li className = "item" >
 						<Link to="/detail/4" className = "title">node 版本升级切换 和 npm 版本升级 mac和windows环境</Link>
+						<p className = "content">
+							快捷键：
+							1：在Sublime Text中，按下Ctrl+Shift+P调出命令面板;
+							2：输入install 调出 Install Package 选项并回车;
+							3：输入pretty，并在列表中选择HTML-CSS-JS Prettify后回车即可安装（格式化 javascript、css、html）
+						</p>
+						<div className = "information">
+							<span className = "time">2017-07-31</span>
+							<span className = "author">晨曦沐枫</span>
+						</div>
 					</li>
 					<li className = "item" >
 						<Link to="/detail/5" className = "title">还在通过伪类来写1px边框吗，那也许换个方案</Link>
+						<p className = "content">
+							快捷键：
+							1：在Sublime Text中，按下Ctrl+Shift+P调出命令面板;
+							2：输入install 调出 Install Package 选项并回车;
+							3：输入pretty，并在列表中选择HTML-CSS-JS Prettify后回车即可安装（格式化 javascript、css、html）
+						</p>
+						<div className = "information">
+							<span className = "time">2017-07-31</span>
+							<span className = "author">晨曦沐枫</span>
+						</div>
 					</li>
 					<li className = "item" >
 						<Link to="/detail/6" className = "title">webkit-tap-highlight-color 解决点击可点击元素的灰色背景，如按钮</Link>
+						<p className = "content">
+							快捷键：
+							1：在Sublime Text中，按下Ctrl+Shift+P调出命令面板;
+							2：输入install 调出 Install Package 选项并回车;
+							3：输入pretty，并在列表中选择HTML-CSS-JS Prettify后回车即可安装（格式化 javascript、css、html）
+						</p>
+						<div className = "information">
+							<span className = "time">2017-07-31</span>
+							<span className = "author">晨曦沐枫</span>
+						</div>
 					</li>
 				</ul>
 			</div>
