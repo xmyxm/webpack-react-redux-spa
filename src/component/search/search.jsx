4348488b1bd1c720034925582e6e23f44a22aea6
xmyxm <574322189@qq.com>
2017-09-14 00:24:08 +0800

    '修改查询页面'

--- src/component/search/search.jsx
+++ src/component/search/search.jsx
@@ -2,10 +2,75 @@ import indexcss from './search.less';
 import ReactDOM from 'react-dom';
 import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
+import *as action from '../../redux/action/fetch-action.js';
+import {connect} from 'react-redux';
+import DateTool from '../../utils/date-format.js';
 
-export default class Home extends Component{
+class Search extends Component{
 	constructor(props){
 		super(props);
+		this.page = 1;
+		this.imgLoading = false;
+		this.dataloading = false;
+		this.totalCount = 0;
+		this.bloglist = [];
+	}
+
+	shouldComponentUpdate(nextProps, nextState){
+		if(nextProps.fetchData){
+    		if(nextProps.fetchData.isFetching) {
+    			this.dataloading = true;
+    			return false;
+    		}
+    		this.dataloading = false;
+    		if(nextProps.fetchData.Json){
+	    		let data = nextProps.fetchData.Json;
+				if(data && data.TotalCount){
+					this.totalCount = data.TotalCount;
+					if(data.PageIndex * data.PageSize >= data.TotalCount){
+						this.imgLoading = false;
+					}else{
+						++this.page;
+						this.imgLoading = true;
+					}
+					if(data.BlogWorkList && data.BlogWorkList.length){
+						this.bloglist = this.bloglist.concat(data.BlogWorkList);
+						return true;
+					}
+				}
+    		}
+    	}
+    	return false;
+	}
+
+	componentWillUnmount() {
+		window.onscroll = null;
+	}
+
+    componentDidMount(){
+    	let _self = this;
+		window.onscroll = (e) => { 
+            if (!_self.imgLoading || _self.dataloading) return;
+
+            if (document.body.scrollTop + window.innerHeight + 150 > document.body.scrollHeight) {
+                _self.pullBlogData();
+            }
+        }
+        _self.pullBlogData();
+    }
+
+	pullBlogData(){
+		this.props.fetchPosts('http://qqweb.top/API/BlogApi/Query',{PageIndex:this.page,key:this.key});
+	}
+
+	Query(){
+		this.page = 1;
+		this.key = this.refs.keyname.value;
+		this.pullBlogData();
+	}
+
+	userEntry(e){
+		//debugger
 	}
 
 	render(){
@@ -14,15 +79,56 @@ export default class Home extends Component{
 				<div className = "head" >
 					<div className = "searchtext" >搜索更懂你！</div>
 					<div className="searchinfo">
-						<input type = "text" name="keyname" className="keytext" value="" />
+						<input type = "text" name="keyname" onInput = {this.userEntry.bind(this)} className="keytext" ref = "keyname" />
 						<i className = "so" ></i>
 						<i className = "del" ></i>
-						<div className = "searchbtn" >搜索</div>
+						<div className = "searchbtn" onClick = {this.Query.bind(this)} >搜索</div>
 					</div>
+				</div>
+				<div className = "listbox">
+					<ul className = "list" >
+					{
+						(this.bloglist.length > 0) && this.bloglist.map(item => {
+							return 	<li key = {item.ID} className = "item" >
+										<Link to={'/detail/' + item.ID} className = "clickarea">
+											<div className = "contenthead">
+												<div className = "title">{item.Title}</div>
+												<div className = "tag">分类:{item.SortName}</div>
+											</div>
+											<p className = "content">{item.Content}</p>
+											<div className = "information">
+												<span className = "time">浏览:{item.PageViewTotal}</span>
+												<span className = "author">{DateTool.ChangeDateFormat(item.UpdateTime)}</span>
+											</div>
+										</Link>
+									</li>
+						})
+					}	
+					</ul>
+					{
+						this.imgLoading ?  <div className="loader">
+												<div className = "pacman">
+													<div></div>
+													<div></div>
+													<div></div>
+													<div></div>
+													<div></div>
+												</div>
+						 					</div>
+						 				:   <div className = "bottominfo" >--- 我是有底线的 ---</div>
+					}
 				</div>
 			</div>
 	    )
 	}
 } 
+
+
+export default connect(
+	state=> {
+		return {fetchData:state.fetchData}
+	}
+	,action)(Search);
+
 
 
