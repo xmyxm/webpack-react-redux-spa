8de6b6a45dcb2f41b009d8e94f185e0206676d95
xmyxm <574322189@qq.com>
2017-10-15 11:43:32 +0800

    '修改打包配置'

--- webpack/webpack.beta.config.js
+++ webpack/webpack.beta.config.js
@@ -9,7 +9,8 @@ config.plugins.push(
 	new ExtractTextPlugin({ filename: 'css/[name].css', disable: true, allChunks: true })
 );
 //source-map的打包可以告诉我们错误源自源码的具体的位置,devtool来选定生成的source-map的详细程度
-config.devtool = 'eval-source-map' //推荐development
+config.devtool = 'eval-source-map'; //推荐development
+config.devtool = 'cheap-module-eval-source-map';
 module.exports = config;
 
 
--- webpack/webpack.pro.config.js
+++ webpack/webpack.pro.config.js
@@ -28,7 +28,8 @@ config.plugins.push(new webpack.optimize.UglifyJsPlugin({
 }));
 
 //source-map的打包可以告诉我们错误源自源码的具体的位置,devtool来选定生成的source-map的详细程度
-config.devtool = 'source-map';//推荐production
+//config.devtool = 'source-map';//
+config.devtool = 'cheap-module-source-map';//生成一个没有列信息（column-mappings）的SourceMaps文件，同时 loader 的 sourcemap 也被简化为只包含对应行的。
 config.output.publicPath = 'http://qqweb.top/m/';//上线配置文件公共路径直接替换为线上域名
 config.output.chunkFilename = 'js/[name]-[chunkhash:8].js';//上线文件打上hash
 module.exports = config;
